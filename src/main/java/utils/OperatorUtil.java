5cc52333f130849741cfce3bae40897384225753
zhu <1>
2020-10-16 16:37:36 +0800

    新增jsonmap工具类

---
+++ src/main/java/utils/OperatorUtil.java
@@ -0,0 +1,59 @@
+package utils;
+
+import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.Expression;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.util.Map;
+
+public class OperatorUtil {
+    private static ScriptEngine engine;
+
+    static {
+        OperatorUtil.engine = null;
+    }
+
+    public static ScriptEngine getEngine() {
+        if (OperatorUtil.engine == null) {
+            try {
+                final ScriptEngineManager manager = new ScriptEngineManager();
+                (OperatorUtil.engine = manager.getEngineByName("javascript")).getBindings(100);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            OperatorUtil.engine.createBindings();
+        }
+        return OperatorUtil.engine;
+    }
+
+    public static Boolean getExp(final String expression, final Map<String, Object> map) {
+        try {
+            Boolean result = false;
+            final Expression compiledExp = AviatorEvaluator.compile(expression);
+            result = (Boolean) compiledExp.execute(map);
+            return result;
+        } catch (Exception ex) {
+            System.out.println("====getExp()====");
+            return false;
+        }
+    }
+
+    public static Boolean getExp(final String expression) {
+        try {
+            final Expression compiledExp = AviatorEvaluator.compile(expression);
+            final Boolean result = (Boolean) compiledExp.execute();
+            return result;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public static void main(final String[] args) {
+        final String topicEventList = "a==1&&b==2&&user_name like '%h%'";
+        final Expression compiledExp = AviatorEvaluator.compile("string.contains('hello','h')");
+        final Boolean execute = (Boolean) compiledExp.execute();
+        System.out.println("execute = " + execute);
+    }
+}
